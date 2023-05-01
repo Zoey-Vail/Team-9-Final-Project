350fc2616d06c488ee59c3bef26b9d59fd0290b8
Zoey-Vail <122924230+Zoey-Vail@users.noreply.github.com>
2023-04-30 21:52:40 -0400

    Add files via upload

--- account_list.sql
+++ account_list.sql
@@ -1,5 +1,12 @@
 CREATE TABLE accounts (
-    username SERIAL       NOT NULL,
+    username VARCHAR(255),
+    passsword VARCHAR(255),
+	email    VARCHAR(255),
+    age    VARCHAR(255),
+    website    VARCHAR(255),
+    gender    VARCHAR(255),
+    major    VARCHAR(255),
+    concentration    VARCHAR(255),
     PRIMARY KEY (username)
 );
 select username
