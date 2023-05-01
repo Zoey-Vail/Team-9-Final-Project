2663c778ddc3c891fc4f7c0a1d0a2e53d32c02bf
Zoey-Vail <122924230+Zoey-Vail@users.noreply.github.com>
2023-04-30 21:51:59 -0400

    Add files via upload

---
+++ movies_schema.sql
@@ -0,0 +1,12 @@
+-- Create movie table
+CREATE TABLE movie (
+    movie_id INT       NOT NULL,
+    title    VARCHAR(255) NOT NULL,
+    director VARCHAR(255) NOT NULL,
+    rating   INT NOT      NULL,
+    PRIMARY KEY (movie_id)
+);
+Select title, movie_id
+from movie;
+
+
